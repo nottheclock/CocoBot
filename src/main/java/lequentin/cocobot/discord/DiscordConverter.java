65940aeb38d43b6cf307f7988ccf0acc6636d498
nottheclock <88286784+nottheclock@users.noreply.github.com>
2023-11-14 16:57:35 -0500

    Fix for unknown messages
    specifically for my old friend dan who unfortunately passed away

--- src/main/java/lequentin/cocobot/discord/DiscordConverter.java
+++ src/main/java/lequentin/cocobot/discord/DiscordConverter.java
@@ -5,11 +5,25 @@ import lequentin.cocobot.domain.User;
 
 public class DiscordConverter {
 
+    private static final String KNOWN_USER_TAG = "dannyp"; // Replace with the actual tag if available
+
     public Message toDomain(discord4j.core.object.entity.Message discordMessage) {
         discord4j.core.object.entity.User discordUser = discordMessage.getAuthor()
-                .orElseThrow(() -> new RuntimeException("Message has no user"));
-        User author = new User(discordUser.getUsername());
+                .orElse(null);
+
+        User author;
+        if (discordUser != null) {
+            // If the author's tag matches the known user tag, map them to "Dan"
+            if (discordUser.getTag().equals(KNOWN_USER_TAG)) {
+                author = new User("Dan");
+            } else {
+                author = new User(discordUser.getUsername());
+            }
+        } else {
+            // Handle the case where the author is not present (user has left the server)
+            author = new User("Unknown");
+        }
+
         return new Message(author, discordMessage.getTimestamp(), discordMessage.getContent());
     }
-
 }
